0bb4781ab2a2a78fd4ecbb2d50de57ec8d7741ac
Roberto <rovermore@gmail.com>
2024-11-18 17:27:57 +0100

    adding some more UI

--- app/src/main/java/com/fottow/fottow/presentation/main/MainScreen.kt
+++ app/src/main/java/com/fottow/fottow/presentation/main/MainScreen.kt
@@ -9,7 +9,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,11 +25,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fottow.fottow.presentation.theme.AppTheme
 import com.fottow.fottow.presentation.theme.FottowTheme
 import com.fottow.fottow.presentation.theme.Typography
 import com.fottow.fottow.presentation.widgets.FTopBar
+import com.fottow.fottow.presentation.widgets.Loader
 import com.fottow.fottow.presentation.widgets.ScreenContainer
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
@@ -35,6 +45,9 @@ import org.koin.androidx.compose.koinViewModel
 fun MainScreen(
     viewModel: MainViewModel = koinViewModel<MainViewModel>()
 ) {
+    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+    val updateSuccessful by viewModel.uploadSuccessful.collectAsStateWithLifecycle()
+
     val context = LocalContext.current
 
     var cameraPhotoUri by remember { mutableStateOf(value = Uri.EMPTY) }
@@ -94,6 +107,36 @@ fun MainScreen(
                     style = Typography.titleLarge
                 )
             }
+        }
+
+        if (updateSuccessful) {
+            FDialog { viewModel.dialogDismissed() }
+        }
+
+        if (isLoading) {
+            Loader()
+        }
+
+    }
+}
+
+@Composable
+fun FDialog(onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Text(
+                text = "Photo has been uploaded successfully",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                textAlign = TextAlign.Center,
+            )
         }
     }
 }
--- app/src/main/java/com/fottow/fottow/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/fottow/fottow/presentation/main/MainViewModel.kt
@@ -4,8 +4,12 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fottow.fottow.FileResolver
+import com.fottow.fottow.domain.base.map
+import com.fottow.fottow.domain.base.then
 import com.fottow.fottow.domain.photo.usecase.UploadPhotoUseCase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel(
@@ -13,16 +17,31 @@ class MainViewModel(
     private val updatePhotoUseCase: UploadPhotoUseCase
 ): ViewModel() {
 
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    private val _uploadSuccessful = MutableStateFlow(true)
+    val uploadSuccessful: StateFlow<Boolean> get() = _uploadSuccessful
 
     fun selectImage(uri: Uri?) {
         viewModelScope.launch(Dispatchers.IO) {
+            _isLoading.value = true
             uri?.let {
                 val imagePath = fileResolver.getRealPathFromUri(it)
                 if (imagePath != null) {
                     updatePhotoUseCase.uploadPhoto(imagePath)
+                        .map {
+                            _uploadSuccessful.value = true
+                        }.then {
+                            _isLoading.value = false
+                        }
                 }
             }
         }
+    }
+
+    fun dialogDismissed() {
+        _uploadSuccessful.value = false
     }
 
 }
