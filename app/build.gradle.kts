5310d01e75c9e0de4fe5300e636feac9936c2ff5
Roberto <rovermore@gmail.com>
2025-08-19 19:59:09 +0200

    upgrading version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.fottow.fottow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25081801
+        versionCode = 25081901
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
